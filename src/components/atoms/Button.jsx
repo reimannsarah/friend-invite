fae5b49b158a6e609effb8ce78a16a3b926a8c11
Sarah Reimann <reimannsarah45@gmail.com>
2023-09-14 16:08:02 -0700

    add className prop to button

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Button = ({ children, onClick }) => {
-  return <button onClick={onClick}>{children}</button>;
+const Button = ({ children, onClick, className, disabled }) => {
+  return <button className={className} onClick={onClick} disabled={disabled}>{children}</button>;
 }
 
 export default Button;
