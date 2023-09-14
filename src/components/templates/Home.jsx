d465af29e6ef6ca50faf184190f03ed91108d8b3
Sarah Reimann <reimannsarah45@gmail.com>
2023-09-14 15:45:13 -0700

    add home template

---
+++ src/components/templates/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "../atoms/Button";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button children={"Invite Friends"}/>
+    </div>
+  )
+}
+  
+export default Home;
+    
