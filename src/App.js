de239c51dcf47c871a924fd3a048415c54e5a7af
Sarah Reimann <reimannsarah45@gmail.com>
2023-09-14 15:45:49 -0700

    add state and conditional for rendering templates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import inviteCandidates from './inviteCandidates.js';
+import Home from './components/templates/Home.jsx';
+import Modal from './components/templates/Modal';
 
 // You can use mock data for building components, then wire up to API later
 console.log("inviteCandidates: ", inviteCandidates)
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
   }
+
+  return (
+    <div>
+      {isModalOpen ? (
+        <Modal />
+      ) : (
+        <Home toggleModal={toggleModal}/>
+      )}
+    </div>
+  )
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <Home />
+//     );
+//   }
+// }
 
 export default App;
