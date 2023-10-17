a8217894b33e03ce9e2bde3dafc387a19892ed19
Sarah Reimann <reimannsarah45@gmail.com>
2023-10-17 14:28:54 -0700

    add useState hook to keep track of friend emails

--- src/components/organisms/FriendsModal.jsx
+++ src/components/organisms/FriendsModal.jsx
@@ -1,18 +1,34 @@
-import React from "react";
-import Friend from "../molecules/Friend";
+import React, { useState } from "react";
 import Button from "../atoms/Button";
 import '../../styles/friends_modal.css'
+// import CheckBox from "../atoms/CheckBox";
+// import NameEmail from "../atoms/NameEmail";
+import Friend from "../molecules/Friend";
 
-const FriendsModal = ({ friends, togglemodal }) => {
+const FriendsModal = ({ friends, toggleModal }) => {
+  const [selectedFriends, setSelectedFriends] = useState([]);
+  {console.log(selectedFriends)}
+  const handleClick = (friend) => {
+    if(!selectedFriends.includes(friend)){
+      setSelectedFriends([...selectedFriends, friend])
+    }else{
+      setSelectedFriends(selectedFriends.filter((f) => f !== friend));
+    }
+  }
+
   return (
     <div className="friends-modal">
-      <Button children={"X"} onClick={togglemodal}/>
-      {friends.map((friend) => {
-        return <Friend friend={friend} />;
-      })}
-      <Button children={"Invite"} className={"btn btn-primary"} />
+      <Button children={"X"} onClick={toggleModal} />
+      <form>
+        {friends.map((friend, index) => {
+          return (
+            <Friend friend={friend} onClick={() => {handleClick(friend.email)}} key={index}/>
+          )
+        })}
+        <Button children={selectedFriends.length == 0 ? "Send Invites" : selectedFriends.length == 1 ? `Send ${selectedFriends.length} Invite` : `Send ${selectedFriends.length} Invites`} className={"btn btn-primary"} disabled={selectedFriends.length == 0}/>
+      </form>
     </div>
   );
-}
+};
 
 export default FriendsModal;
