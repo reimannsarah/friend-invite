47a5ab17872fe1faa648cd87e1164222436d6a39
Sarah Reimann <reimannsarah45@gmail.com>
2023-10-19 14:50:17 -0700

    change name of prop

--- src/components/templates/Home.jsx
+++ src/components/templates/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Button from "../atoms/Button";
 
-const Home = ({toggleModal}) => {
+const Home = ({seeFriends}) => {
   return (
     <div>
       <h1 className="App-title">Home</h1>
-      <Button children={"Invite Friends"} className={"btn btn-primary"} onClick={toggleModal}/>
+      <Button children={"Invite Friends"} className={"btn btn-primary"} onClick={seeFriends}/>
     </div>
   )
 }
