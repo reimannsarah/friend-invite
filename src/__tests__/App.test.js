f2c601b1bb743e893e5a0ca60470adefbc3d4e73
Sarah Reimann <reimannsarah45@gmail.com>
2023-10-19 14:55:05 -0700

    passing test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -11,4 +11,19 @@ test('should return an array of friends', async () => {
   });
   const friends = await response.json();
   expect(Array.isArray(friends)).toBe(true);
+});
+
+test('should return a message saying how many friends were invited', async () => {
+  const response = await fetch(`${url}/friends/invite`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': `${process.env.API_KEY}`
+    },
+    body: JSON.stringify(['crank@panda.com', 'salad@candy.net']),
+  });
+
+  const data = await response.json();
+  const message = 'You have invited 2 friend(s)!'
+  expect(data.message).toEqual(message);
 })
