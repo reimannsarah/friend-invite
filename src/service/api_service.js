aa1e29aaaabc661888c0c72fd3e7bb076751ee1b
Sarah Reimann <reimannsarah45@gmail.com>
2023-10-19 17:28:54 -0700

    add POST method

--- src/service/api_service.js
+++ src/service/api_service.js
@@ -1,6 +1,6 @@
 const url = 'https://friend-invite-express-server-f35149be8b17.herokuapp.com'
 
-export default async function getFriends() {
+export async function getFriends() {
   const response = await fetch(`${url}/friends`, {
     headers: {
       'x-api-key': 'NOFRIENDS'
@@ -8,4 +8,23 @@ export default async function getFriends() {
   });
   const data = await response.json();
   return data;
+}
+
+export async function inviteFriends(friendsArr) {
+  try{
+  const response = await fetch(`${url}/friends/invite`, {
+    method: 'POST',
+    headers: {
+      'x-api-key': 'NOFRIENDS',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(friendsArr),
+  });
+  const data = await response.json();
+  console.log(data);
+  return data.message;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
